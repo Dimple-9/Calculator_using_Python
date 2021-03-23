df27223b32eba7b08168a0b6f48ccb30c6a83434
Dimple-9 <78236662+Dimple-9@users.noreply.github.com>
2021-03-22 20:25:07 -0400

    Task: Added Calculator.py

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,11 +1,49 @@
+import math
+
+
 def addition(a, b):
-    return a + b
+    a = int(a)
+    b = int(b)
+    c = a + b
+    return c
+
 
 def subtraction(a, b):
-    return a - b
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
+
+def multiplication(a, b):
+    a = int(a)
+    b = int(b)
+    c = a * b
+    return c
+
+
+def division(a, b):
+    a = int(a)
+    b = int(b)
+    c = float(b / a)
+    return c
+
+
+def square(a):
+    a = int(a)
+    c = a * a
+    return c
+
+
+def sqrt(a):
+    a = int(a)
+    c = round(math.sqrt(a), 7)
+    return c
+
 
 class Calculator:
- result = 0
+    result = 0
+
     def __init__(self):
         pass
 
@@ -13,6 +51,22 @@ class Calculator:
         self.result = addition(a, b)
         return self.result
 
-    def subtract(self, a, b):
+    def sub(self, a, b):
         self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = round(division(a, b), 9)
+        return self.result
+
+    def squares(self, a):
+        self.result = square(a)
+        return self.result
+
+    def square_root(self, a):
+        self.result = sqrt(a)
         return self.result
